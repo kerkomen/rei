1d863a3875351ee775953cc6834ac147759fba47
Daniel <@>
2015-07-22 23:57:00 +0300

    Initial commit

---
+++ Main.hs
@@ -0,0 +1,458 @@
+#!/usr/bin/env runhaskell
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import System.Environment(getArgs)
+import System.Console.GetOpt
+import System.Exit
+import System.IO(stderr, hPutStrLn)
+import System.Directory(doesFileExist)
+import Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString.Char8 as B
+import Data.Word
+
+import Text.Regex.Posix((=~))
+import Data.List(isSuffixOf, isInfixOf, findIndex, findIndices, elem, nub, maximumBy, groupBy)
+import Data.List.Split(splitOn, splitWhen)
+import Data.Char(isLetter, isNumber, isSymbol, isSeparator, isPunctuation)
+import Data.Map(fromListWith, toList)
+import Data.Function(on)
+
+import Control.Monad(when, unless)
+import Data.Either(rights)
+
+import Text.Read(readMaybe)
+
+-- | Chapter 1. Functions for parsing the rule
+
+data Rule = Rule [Char] [Char]
+	deriving Show
+
+parseRule :: Parser Rule
+parseRule = do
+	left  <- many' anyChar
+	string "->"
+	right <- many' anyChar
+	return $ Rule left right
+
+-- | Guess the delimiter for several known formats
+
+knownFormats = [".csv", ".tsv", ".ssv"]
+
+decideF :: FilePath -> Maybe Char
+decideF flnm
+	| ".csv" `isSuffixOf` flnm = Just ','
+	| ".tsv" `isSuffixOf` flnm = Just '\t'
+	| ".ssv" `isSuffixOf` flnm = Just ' '
+	| otherwise 		   	   = Nothing
+
+guessF :: String -> Char
+guessF fstLine = mostFreqSymbol
+	where
+		mostFreqSymbol = fst $ maximumBy (compare `on` snd) symbolCount
+		symbolCount    = filter (\(s,n) -> isSymbol s || isSeparator s || isPunctuation s) freqMap
+		freqMap        = toList $ fromListWith (+) [(c, 1) | c <- fstLine]
+
+
+tuplify2 :: [t] -> (t, t)
+tuplify2 (a:b:_) = (a,b)
+
+isEmpty :: Char -> Bool
+isEmpty x = (x == ' ') || (x == '\t')
+
+lstrip :: [Char] -> [Char]
+lstrip []       = []
+lstrip w@(x:xs) = if (isEmpty x) then (lstrip xs) else w
+
+rstrip :: [Char] -> [Char]
+rstrip [] = []
+rstrip xs = if (isEmpty $ last xs) then rstrip (init xs) else xs
+
+strip :: [Char] -> [Char]
+strip = lstrip . rstrip
+
+
+isFieldVar :: [Char] -> Bool
+isFieldVar = all (\x -> isLetter x || isNumber x)
+
+isFieldChar :: Char -> Bool
+isFieldChar x = isLetter x || isNumber x
+
+
+interDivise x = splitWhen (not . isFieldChar) x
+
+fi :: Eq a => [a] -> a -> [Int]
+fi ar el = findIndices (==el) ar
+
+findFstInSnd :: Eq a => [a] -> [a] -> [[Int]]
+findFstInSnd a b = map (fi b) a
+
+sole :: Eq a => [a] -> [a] -> Bool
+sole x xs = length (splitOn x xs) == 2
+
+sole' :: Eq a => [a] -> [a] -> Bool
+sole' x xs = length (splitOn x xs) <= 2
+
+splitMargins :: Eq a => a -> [a] -> (Int, Int)
+splitMargins x xs = (pos', length xs - pos' - 1)
+	where
+		Just pos'
+			| pos == Nothing = Just 0
+			| otherwise      = pos
+		pos       = findIndex (==x) xs
+
+
+-- | Check the rule correctness
+-- 1. Every variable on the right must be present on the left
+
+checkRule list1 list2
+	| ell `isInfixOf` list1 = (all (flip elem list1) list2) 
+						      && (sole  ell list1)
+	| otherwise             = all (flip elem list1) list2
+		where
+			ell = "..."
+
+
+-- | Chapter 2. Functions for parsing the records
+
+recordWithSep sep field = sepBy field sep
+
+anyField sep = takeTill (==sep)
+
+parseLine line sep = feed (parse (recordWithSep (char sep) (anyField sep)) $ B.pack line) B.empty
+
+parseLine' line sep = y
+	where Done x y = case sep of
+		Nothing   -> parseLine line (guessF line)
+		Just sep' -> parseLine line sep'
+
+packEither oldDelim newDelim line = case newDelim of
+	Nothing -> tryOld oldDelim line
+	Just x  -> B.pack [x]
+
+tryOld oldDelim line = case oldDelim of 
+	Nothing -> B.pack [guessF line]
+	Just y  -> B.pack [y]
+
+maybeEnumerate enumBool oldDelim newDelim
+	| enumBool  = zipWith (++) (map (\x -> show x ++ [delim]) [1..])
+	| otherwise = id
+		where
+			Just old' = oldDelim
+			Just new' = newDelim
+			delim
+				| oldDelim == Nothing && newDelim == Nothing = error "Provide a new delimiter to enumerate lines."
+				| newDelim == Nothing = old'
+				| otherwise = new'
+
+-- | Chapter 3. Parse options and flags
+
+data Options = Options  {
+	optVersion  :: Bool,
+	optHelp     :: Bool,
+
+	optVerbose  :: Bool,
+	optDelim    :: Maybe Char,
+	optNewDelim :: Maybe Char,
+
+	optSkip :: Maybe Int,
+	optOmit :: Maybe Int,
+	optEnum :: Bool
+	}
+
+-- Handy if only version or help is needed
+defaultOptions' = defaultOptions "example.ssv"
+
+defaultOptions :: FilePath -> Options
+defaultOptions filename = Options {
+	optVersion  = False,
+	optHelp     = False,
+
+	optVerbose  = False,
+    optDelim    = decideF filename,
+    optNewDelim = decideF filename,
+
+    optSkip = Just 0,
+    optOmit = Just 0,
+    optEnum = False
+}
+
+options :: [OptDescr (Options -> IO Options)]
+options = [ Option ['V'] ["version"]  (NoArg showVersion)  	   	"Show version number",
+			Option ['h'] ["help"]     (NoArg showHelp)  	   	"Show help message",
+			Option ['v'] ["verbose"]  (NoArg setVerbose)  	   	"Enable verbose messages",
+			Option ['f'] ["delim"]    (ReqArg setDelim " ")     "Define delimiter in the input file",
+			Option ['g'] ["newdelim"] (ReqArg setNewDelim " ")  "Define delimiter in the output file",
+			Option ['s'] ["skip"] 	  (ReqArg setSkip "0")     	"Set number of lines to skip",
+			Option ['t'] ["omit"] 	  (ReqArg setOmit "0")     	"Set number of lines to omit in the end of the file",
+			Option ['n'] ["enum"] 	  (NoArg setEnum)     		"Treat the line number as the first column of the input."
+		  ]
+
+setDelim    arg opt = return opt { optDelim = if length (readLiteral arg) > 0 
+												then Just (readLiteral arg !! 0) 
+												else error "The delimiter should have non-zero length"
+								 }
+
+setNewDelim arg opt = return opt { optNewDelim = if length (readLiteral arg) > 0 
+													then Just (readLiteral arg !! 0) 
+													else error "The new delimiter should have non-zero length" }
+
+
+setSkip arg opt = return opt { optSkip = let argParsed = readMaybe arg 
+											in if (argParsed == Nothing)
+							       			       then error "The number of lines to skip should be an integer" 
+							       			   	   else argParsed
+							 }
+
+setOmit arg opt = return opt { optOmit = let argParsed = readMaybe arg 
+											in if (argParsed == Nothing)
+							       			       then error "The number of lines to omit should be an integer" 
+							       			   	   else argParsed
+							 }
+
+setEnum opt = return opt { optEnum = True }
+
+
+setVerbose opt = return opt { optVerbose = True }
+
+elem' :: Eq a => [a] -> a -> Bool
+elem' = flip elem
+
+readLiteral :: [Char] -> String
+readLiteral s = read ('"' : s ++ "\"") :: String
+
+parseArgs argv = case getOpt Permute options argv of
+ 
+        (args,fs,[]) -> do
+            let files = if null fs then ["-"] else fs
+            return (args, files)
+ 
+        (_,_,errs)	 -> do
+            hPutStrLn stderr (concat errs ++ usageInfo header options)
+            exitWith (ExitFailure 1)
+ 
+        where header = "Usage: rei [options] rule file"
+
+showVersion _ = do
+	hPutStrLn stderr "rei: process lists easily. Version 0.1 pre-alpha. July 2015."
+	exitWith ExitSuccess
+
+showHelp _    = do
+	let header = "\n\trei is designed to process lists easily\n"
+	hPutStrLn stderr (usageInfo header options)
+	exitWith ExitSuccess
+
+
+
+-- | Chapter 4. Working with the file
+
+unlessM :: Monad m => m Bool -> m () -> m ()
+unlessM b s = b >>= (\t -> unless t s)
+
+
+
+-- | Chapter 5. Magic functions
+
+condense2 :: Eq a => [[a]] -> [[a]]
+condense2 input = zipWith (:) keys values
+	where 
+		grouped = groupBy ((==) `on` fst) . map tuplify2 $ input
+		keys    = map (fst . (!! 0)) grouped
+		values  = map (map snd) grouped
+
+melter :: [t] -> [[t]]
+melter []       = []
+melter (a:b:[]) = [ [a,b] ]
+melter (a:b:xs) = ( [a,b]:(melter (a:xs)) )
+
+melt2 :: [[t]] -> [[t]]
+melt2 input = foldr (++) [] . map melter $ input
+
+
+main = do
+	(opts, args) <- getArgs >>= parseArgs
+	-- putStrLn $ "Flags: " ++ show opts
+
+	-- Check several special options: help and version
+	optsSp <- foldl (>>=) (return defaultOptions') opts
+	let Options {
+			  optVersion = versionOnly,
+			  optHelp    = helpOnly     } = optsSp
+
+	when helpOnly    $ showHelp ""
+	when versionOnly $ showVersion ""
+
+	-- Check the rule
+	let (rule:files) = args
+	let filename
+		| rule == "-"      = error "Provide a rule and a filename\n--  Example: `rei \"a b -> b a\" example.ssv`"
+		| length files > 0 = files !! 0
+		| otherwise        = error "Provide a filename\n--  Example: `rei \"a b -> b a\" example.ssv`"
+
+	-- Parse options
+	opts' <- foldl (>>=) (return $ defaultOptions filename) opts
+	let Options { 
+			  optVerbose  = verbose
+			, optVersion  = versionOnly
+			, optDelim    = fieldSep
+            , optNewDelim = fieldSep'
+            , optSkip     = linesToSkip
+            , optOmit     = linesToOmit
+            , optEnum     = ifEnumerate } = opts'
+
+    -- Check if the delimiter provided if the file format is unknown
+   	if fieldSep == Nothing 
+   		then hPutStrLn stderr "Warning: the delimiter is not provided and can not be deduced from the file extension."
+   		else return ()
+
+	-- Parse the rule
+	let pat = "(.*[^\\])->(.*)" :: String
+
+	-- Definitions for magic functions
+	let finalDelim
+		| fieldSep' /= Nothing = B.pack [d2]
+		| fieldSep /= Nothing  = B.pack [d1]
+		| otherwise            = B.pack " "
+			where
+				Just d1 = fieldSep
+				Just d2 = fieldSep'
+	let open x = if x == "-" then getContents else readFile x
+	let drop' (Just n) = drop n
+	let take' m xs
+		| m == Just 0 = id xs
+		| otherwise   = Prelude.take (length xs - m') xs
+			where
+				Just m' = m
+	let getParsed x = parseLine' x fieldSep
+
+	-- Create a union of two files, column-wise
+	when (strip rule == "unite") $ do
+		when verbose $ hPutStrLn stderr "Uniting files..."
+		let filename2
+			| length files > 1 = files !! 1
+			| otherwise        = error "Provide two filenames to unite the files\n--  Example: `rei unite f1.ssv f2.ssv`"
+		let print' x = B.putStrLn $ B.intercalate finalDelim $ x
+		let parseActions = map getParsed . take' linesToOmit . drop' linesToSkip . lines
+		leftFile  <- open filename
+		rightFile <- open filename2
+		mapM_ print' $ map (\(x,y) -> x++y) $ zip (parseActions leftFile) (parseActions rightFile)
+		exitWith ExitSuccess
+
+	-- Condense the first two columns of the file
+	when (strip rule == "condense2") $ do
+		when verbose $ hPutStrLn stderr "Condensing a file..."
+		let print' x = B.putStrLn $ B.intercalate finalDelim $ x
+		mapM_ print' . condense2 . map getParsed . take' linesToOmit . drop' linesToSkip . lines =<< open filename
+		exitWith ExitSuccess
+
+	-- Melt the file into two-column file
+	when (strip rule == "melt2") $ do
+		when verbose $ hPutStrLn stderr "Melting a file..."
+		let print' x = B.putStrLn $ B.intercalate finalDelim $ x
+		mapM_ print' . melt2 . map getParsed . take' linesToOmit . drop' linesToSkip . lines =<< open filename
+		exitWith ExitSuccess
+
+	let (before, after) 
+		| matchResult == [] = error "Something's wrong with the rule. The rule must have two sets of column descriptors separated by the arrow.\n-- Example: `rei \"a b -> b a\" example.ssv`"
+		| otherwise         = tuplify2 . drop 1 $ matchResult !! 0
+			where 
+				matchResult = (rule =~ pat :: [[String]])
+	-- print $ splitOn " " rule
+	-- print $ strip before
+	-- print $ strip after
+	let ell = "..."
+
+	let order
+		| checkRule before after = concat $ findFstInSnd (words after) (words before)
+		| sole' ell before && sole' ell after = error "Something's wrong with the rule. There might be a variable which is unique to the right part."
+		| otherwise = error "Something's wrong with the rule. There might be multiple ellipsis signs."
+				
+
+	-- TODO: remove
+	--let correctOrder' ns xs = before_ell ++ inside_ell ++ after_ell
+	--	where
+	--		(k, _)       = splitMargins ell (words after) 
+	--		(prex, sufx) = splitMargins ell (words before)
+	--		before_ell
+	--			| rightEllipsis  = map (proceed shift) $ Prelude.take k ns
+	--			| otherwise      = map (proceedSkip prex) ns 
+	--		inside_ell 
+	--			| rightEllipsis  = [ prex .. lxs - sufx - 1 ]
+	--			| otherwise 	 = []
+	--		after_ell  
+	--			| rightEllipsis  = map (proceed shift) $ drop (k+1) ns
+	--			| otherwise      = []
+	--		proceed s x     = if x > prex then x+s else x
+	--		shift           = lxs - prex - sufx - 1
+	--		proceedSkip a x = if x <= a then x else x + shift
+	--		lxs             = length xs
+	--		rightEllipsis   = sole ell after
+	
+	let correctOrder ns xs = ifEl
+		where
+			(prex, sufx) = splitMargins ell (words before)
+			ifEl
+				| rightEllipsis  = concat $ map (proceed shift) ns
+				| otherwise      = map (proceedSkip prex) ns 
+			proceed s x     = if x > prex then [x+s] 
+								else if x == prex then [ x .. x + shift ]
+									else [x]
+			shift           = lxs - prex - sufx - 1
+			proceedSkip a x = if x <= a then x else x + shift
+			lxs             = length xs
+			rightEllipsis   = ell `isInfixOf` after
+
+	-- Parse the records
+	let 
+		print' x
+			| length order == 0 = error "The right part is empty. That doesn't make sense..."
+			| maximum order >= length (getParsed x) = error "Too many fields requested. There might be extra fields in the rule, or the wrong delimiter might have been used."
+			| otherwise = B.putStrLn $ B.intercalate (packEither fieldSep fieldSep' x) $ getFields x
+
+		getParsed x = parseLine' x fieldSep
+		getFields x
+			| ell_pos == Nothing = map (pre_x !!) order
+			| otherwise          = map (pre_x !!) $ correctOrder order pre_x
+				where
+					ell_pos       = findIndex (=="...") (words before)
+					pre_x         = getParsed x
+
+	-- Read the file and apply the rule
+	let withFile s = mapM_ print' . maybeEnumerate ifEnumerate fieldSep fieldSep'
+								  . take' linesToOmit
+								  . drop' linesToSkip
+								  . lines =<< open s
+		where
+			open x = if x == "-" then getContents else readFile x
+			drop' (Just n) = drop n
+			take' m xs
+				| m == Just 0 = id xs
+				| otherwise   = Prelude.take (length xs - m') xs
+					where
+						Just m' = m
+
+	withFile filename
+
+	-- Verbose messages
+
+	when verbose $ hPutStrLn stderr (case fieldSep of
+				Nothing    -> "Delimiter of the input file is not defined."
+				Just delim -> "-- Delimiter of the input file: \'" ++ [delim] ++ "\'.")
+
+	when verbose $ hPutStrLn stderr (case fieldSep' of
+				Nothing    -> "-- Delimiter of the output file is not defined."
+				Just delim -> "-- Delimiter of the output file: \'" ++ [delim] ++ "\'.")
+
+
+	when verbose $ hPutStrLn stderr (case linesToSkip of
+				Just 0 -> "-- No lines skipped in the beginning of the file."
+				Just x -> "-- Lines skipped in the beginning: \'" ++ show linesToSkip ++ "\'.")
+
+	when verbose $ hPutStrLn stderr (case linesToOmit of
+				Just 0 -> "-- No lines skipped in the end of the file."
+				Just x -> "-- Lines omitted in the end: \'" ++ show linesToOmit ++ "\'.")
+
+	
+	when verbose $ hPutStrLn stderr (case ifEnumerate of
+				True  -> "-- Line enumeration is enabled."
+				False -> "-- Lines are not enumerated." )
--- README.md
+++ README.md
@@ -1,2 +1,241 @@
-# rei
-Process lists easily
+# Process lists easily with `rei`
+
+While originally *rei* was not intended to be an abbreviation, one may think of it as of the *Row Editing Interface*. Working with lists is an important part of many people, including data-scientists and bioinformaticians, and `rei` aims to make that experience more pleasant.
+
+## Installation
+
+TODO
+
+## Getting started
+
+```
+rei                      "rname x y -> rname y x" example.csv
+rei unite            example_left.csv example_right.csv
+rei melt2          example_condensed.csv
+rei condense2 example_melted.csv
+```
+
+### Defining the rule 
+
+The main idea of the `rei` is to apply the **rule** over the lines of the file. The rule should consist of two parts — *before* and *after* — separated by the arrow sign («->»). The *before* part of the rule describes the fields (columns) in one record (line) in the initial file. The *after* part of the rule describes the desired format of the output.
+
+The arrow sign should be surrounded with spaces. Like this: `.. -> ..`. The fields in the rule should be surrounded with spaces too. The field delimiter in the output file is the same as in the input file by default, however it's possible to change it via the option `-g`, or `--newdelim`. (It's easy to remember, since `-f` is the flag to set the delimiter in the input file.)
+
+### Providing the file
+
+There's several ways to provide `rei` with the content of the file. The first one is the-most-obvious-way-you-can-think-of: just provide the path to the file. Sometimes it is helpful to use process substitution (? TODO). And if there's a need to pipe the content, just write a dash («-»). Well, here's the code:
+
+```sh
+> rei "x -> x" 0.ssv
+...
+> rei "x -> x" <(cat 0.ssv)
+...
+> cat 0.ssv | rei "x -> x" -
+...
+```
+
+### Simple examples
+
+Let's use a small sample file with spaces as delimiters for these examples (saved as `0.ssv`):
+
+```
+A B C
+D E F
+G H I
+J K L
+M N O
+P Q R
+S T U
+V W X
+Y Z 0
+```
+
+This is how easily we can address the columns:
+
+```sh
+> rei "a b c -> c b a" 0.ssv
+C B A
+F E D
+I H G
+L K J
+O N M
+R Q P
+U T S
+X W V
+0 Z Y
+```
+
+The columns are now in the reversed order.
+
+We can extract the columns that we need:
+
+```sh
+> rei "a b c -> b" 0.ssv
+B
+E
+H
+K
+N
+Q
+T
+W
+Z
+```
+
+It is possible to define only columns needed:
+
+```sh
+> rei "a b -> a b" 0.ssv
+A B
+D E
+G H
+J K
+M N
+P Q
+S T
+V W
+Y Z
+```
+
+You may want to keep the rest of the columns, here's how you can accomplish that:
+
+```sh
+> rei "a b ... -> a ..."
+A C
+D F
+G I
+J L
+M O
+P R
+S U
+V X
+Y 0
+```
+
+The beauty is that one may give columns descriptive titles. And that is great in so many ways, as it increases readability, productivity, descriptiveness, maintainability and awareness of what's happening with all that list processing. See some real-world examples below.
+
+### Keywords
+
+You can define a delimiter (`-f`, or `--delim`, for the input file and  `-g`, or `--newdelim`, for the output file). It's important to emphasize that only one-character long delimiters are used. Tabulation («\t») is considered one-character too. If multicharacter literal is provided, `rei` uses its first symbol as a delimiter.
+
+For some common file formats `rei` doesn't require a delimiter to be provided individually:
+
+* .ssv &rarr; space (' '),
+* .csv &rarr; comma (','),
+* .tsv &rarr; tab ('\t'),
+* .txt &rarr; space (' ').
+
+TODO
+
+The flag `-g` is powerful as it allows for fast format conversion. That's how `rei` may be used to convert from .ssv to .csv:
+
+```sh
+> rei -g ","  "... -> ..." 0.ssv
+A,B,C
+D,E,F
+G,H,I
+J,K,L
+M,N,O
+P,Q,R
+S,T,U
+V,W,X
+Y,Z,0
+```
+
+As you see, `rei` guessed the delimiter in the input file by its extension — *space-separated values*. The output won't change in the example above if we provide `-f " "`.
+
+
+TODO
+
+There's also flags to define the number of lines to skip in the beginning (`--skip`, or `-s`) or in the end (`--omit`, or `-t`) of the file.
+
+TODO
+
+### Skipping lines
+
+Sometimes there is a need to cut out the header of the file or several lines in its end. It's generally accomplished by combining `head` and/or `tail` programs, piping, etc. Since `rei` is designed for easy list processing, such feature is implemented here.
+
+
+
+### Magic rules
+
+
+There's are some common tasks that one may want to do with lists and tables, and it seems convenient to include them in `rei`: melt2, condense2, unite. Each *magic rule* has its own syntax.
+
+TODO
+
+#### **Melt**ing and **condens**ing
+
+TODO
+
+
+
+#### **Unit**ing
+
+TODO
+
+### Outputting to file
+
+The default behaviour for `rei` is to give the output to `stdout`. That allows `rei` to be easily embedded  into workflows and pipelines. 
+
+However there's a common case when files are given suffixes or prefixes while mining the data, like `brain_data.csv` and `brain_data_adults_only.csv`. There's an option to shorten the way it can be written in `rei`:
+
+```sh
+rei "x y z -> z" testfile_in.csv > testfile_out.csv
+rei "x y z -> z" "testfile_(in -> out).csv"
+```
+
+The two examples above are almost equivalent, but when using *naming pattern* `(in -> out)` the output file should not exist, otherwise `rei` throws an error with the corresponding message. *Naming pattern* also makes the command shorter and the idea behind it easier to understand:
+
+```sh
+rei "rn _ _ expr3 -> rn expr3" brain_expression(_all -> _adults).csv
+```
+
+No space characters, or `<` / `>`, or parenthesis are allowed in file names if *naming pattern* is used.
+
+### Sophisticated examples
+
+Skip rownames and colnames, than calculate mean of every row, than append the new column to the file from the second row, and to the first row the title "mean" is added:
+
+```sh
+> rei --skip 1 "rownames ... -> ... => mean" | \
+       rei --magic "unite" --colnames "<< Mean" example.ssv -
+```
+
+It's easy to merge several files, and turn the output to .csv:
+
+```sh
+> cat <(rei "a b c -> a c" 0.ssv) <(rei "x y z -> y z" 1.ssv) | rei -f " " -g "," "1 2 -> 1 2" -
+TODO
+```
+
+### Real-world examples
+
+- .bam files stats
+- date and smth else
+- uniting data
+- merging data
+
+## Some more notes
+
+### Errors and warnings
+
+`rei` tries to be friendly to the user. For example, when there's a field variable in the *right* part of the rule that is not present in the *left* part, `rei` hides implementation details behind the user-friendly message, trying to guess that *Something's wrong with the rule...*
+
+TODO
+
+## Dev
+
+`Rei` is written in Haskell, uses [regular expressions](http://hackage.haskell.org/package/regex-posix) to parse the rule and [Attoparsec](https://hackage.haskell.org/package/attoparsec) to parse the file provided.
+
+### Requests
+
+* guessing delimiters for "bioinformatic" formats, like .sam, .vsf, etc.
+
+### Sources
+
+* [Parsec examples](https://github.com/JakeWheat/intro_to_parsing)
+* [Parsec to parse files](https://www.fpcomplete.com/school/starting-with-haskell/libraries-and-frameworks/text-manipulation/attoparsec)
+* [Faster than C++](http://newartisans.com/2012/08/parsing-with-haskell-and-attoparsec/)
+* [Argument handling in Haskell](https://wiki.haskell.org/Tutorials/Programming_Haskell/Argument_handling)
+* [Command line options in Haskell](http://leiffrenzel.de/papers/commandline-options-in-haskell.html)
